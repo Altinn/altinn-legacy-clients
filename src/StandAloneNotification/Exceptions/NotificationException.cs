07a96d7721097505c81529c31f6b5167f9def271
Kʜᴀɴ <codemascot@hotmail.com>
2024-02-21 10:11:26 +0100

    #29 Upgrade to .NET 8 (#34)
    * Packages upgraded to .NET 8
    
    * MinVerDefaultPreReleasePhase -> MinVerDefaultPreReleaseIdentifiers
    
    * Non-nullable values made nullable

--- src/StandAloneNotification/Exceptions/NotificationException.cs
+++ src/StandAloneNotification/Exceptions/NotificationException.cs
@@ -2,19 +2,19 @@
 {
     public class NotificationException : Exception
     {
-        public string AltinnErrorMessage;
+        public string? AltinnErrorMessage;
 
-        public string AltinnExtendedErrorMessage;
+        public string? AltinnExtendedErrorMessage;
 
-        public string AltinnLocalizedErrorMessage;
+        public string? AltinnLocalizedErrorMessage;
 
-        public string ErrorGuid;
+        public string? ErrorGuid;
 
         public int ErrorID;
 
-        public string UserGuid;
+        public string? UserGuid;
 
-        public string UserId;
+        public string? UserId;
 
         public NotificationException() { }
         public NotificationException(string message) : base(message) { }
